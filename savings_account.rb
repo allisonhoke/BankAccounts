4aa8e62413b63a3b6f05a6e2ee06739df47e2005
Allison Hoke <allisonhoke@gmail.com>
2016-08-25 15:08:33 -0700

    created add_interest method

--- savings_account.rb
+++ savings_account.rb
@@ -1,28 +1,27 @@
-class SavingsAccount < Account # inherit from Account class
+require_relative 'account'
 
-  def initialize(balance) # initialize with a balance
-    @balance = balance
+module Bank
+  class SavingsAccount < Account # inherit from Account class
 
-    if @balance < 1000 # if balance is less than 10, raise argument error
-      raise ArgumentError.new("Must deposit at least $10.00.")
+    def initialize(balance) # initialize with a balance
+      @balance = balance
+      @minimum_balance = 1000
+
+      if @balance < @minimum_balance #??CAN THIS BE SUPER(ed) WITHOUT HASH INPUT IN ACCOUNT MUCKING IT ALL UP?
+        raise ArgumentError.new("Cannot create an account with less than #{@minimum_balance}.")
+      end
+    end
+
+    def withdraw(amount)
+      fee = 200 # each transaction incurs a fee of $2
+      amount += fee # include $2 fee in amount
+      super # inherit conditional to determine if there is enough in account to make this withdraw
+    end
+
+    def add_interest(rate)
+      interest = @balance * rate / 100 #var. holds calculated interest
+      @balance += interest #update balance
+      return interest #return the amount of interest
     end
   end
-
-  def withdraw(amount)
-    super
-    fee = 200 # each transaction incurs a fee of $2
-    total_with_fee = amount + fee
-    if @balance - total_with_fee < 1000
-      puts "Sorry, your balance cannot go below $10. Transaction cancelled."
-      return @balance # output a warning message and return un-modified amount if balance will drop below $10 (including fee)
-    else
-      return @balance -= total_with_fee
-    end 
-  end
 end
-
-
-
-# add interest method
-# create variable to hold calculated interest
-# return the interest that was calculated
