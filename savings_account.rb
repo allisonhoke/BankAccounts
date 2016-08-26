aba0c687a8bb08848eb4952a66cc637a51d7e367
Allison Hoke <allisonhoke@gmail.com>
2016-08-26 14:55:29 -0700

    deleted commented out trial methods that served no purpose

--- savings_account.rb
+++ savings_account.rb
@@ -27,39 +27,4 @@ end
 #
 # #TESTS for error
 # b = Bank::SavingsAccount.new({:id => 1235, :balance => 900, :open_date => "1997-11-17 14:04:56 -0800"})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#     def self.all
-#       accounts = []
-#       CSV.read("savings.csv").each do |line|
-#         account = {} #create empty hash to store this CSV line's data
-#
-#         account[:id] = line[0].to_i
-#         account[:balance] = line[1].to_i
-#         account[:open_date] = line[2]
-#
-#         accounts << self.new(account) #create new instance with hash info
-#       end
-#       accounts #returns an array of object: Account instances
-#     end
-#
-# #THIS DOESN'T WORK
-#     def self.find(id)
-#       self.all.each do |o|
-#         if o.id == id
-#           return o #returns an object: Account
-#         end
-#       end
-#     end
+  
